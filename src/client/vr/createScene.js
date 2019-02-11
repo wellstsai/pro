dcda02cc32704f497b597857bec5fb975050883c
Wells Tsai <wells.z.tsai@gmail.com>
2019-02-10 22:29:28 -0600

    roof wip

--- src/client/vr/createScene.js
+++ src/client/vr/createScene.js
@@ -13,31 +13,16 @@ export const createScene = (engine, canvas) => {
   //   camera.attachControl(canvas, true);
   // }
 
-  // Parameters : name, position, scene
-  var camera = new BABYLON.UniversalCamera('UniversalCamera', new BABYLON.Vector3(0, 0, 10), scene);
+  
 
-  // Targets the camera to a particular position. In this case the scene origin
-  camera.setTarget(BABYLON.Vector3.Zero());
-
-  // Attach the camera to the canvas
-  camera.attachControl(canvas, true);
 
   scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
-  camera.applyGravity = true;
 
-  camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
 
   scene.collisionsEnabled = true;
-  camera.checkCollisions = true;
 
   // Create a basic light, aiming 0,1,0 - meaning, to the sky.
   const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-
-  // Create a built-in "sphere" shape. 
-  const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {segments:16, diameter:2}, scene);
-
-  // Move the sphere upward 1/2 of its height.
-  sphere.position.y = 1;
 
   // Create a built-in "ground" shape.
   // const ground = BABYLON.MeshBuilder.CreateGround('ground1', {height:6, width:6, subdivisions: 2}, scene);
@@ -57,7 +42,7 @@ export const createScene = (engine, canvas) => {
 
   // var ground = BABYLON.MeshBuilder.CreateGround('ground', {}, scene); //default ground
 
-  var ground = BABYLON.MeshBuilder.CreateGround('myGround', {width: 600, height: 400}, scene);
+  var ground = BABYLON.MeshBuilder.CreateGround('myGround', {width: 10000, height: 10000}, scene);
   ground.checkCollisions = true;
   // // Sky material
   // var skyboxMaterial = new BABYLON.SkyMaterial('skyMaterial', scene);
@@ -71,38 +56,93 @@ export const createScene = (engine, canvas) => {
 
 
   // create maze
-  const wallSizeFactor = 1.8;
-  const wall = BABYLON.Mesh.CreateBox('plane', wallSizeFactor, scene); // this wall is root to make instances off of
+
+  // create a wall
+  const wallSizeFactor = 15;
+  const wall = BABYLON.Mesh.CreateBox('plane', 1, scene); // this wall is root to make instances off of
   wall.checkCollisions = true;
-  wall.scaling.x = 1;
-  wall.scaling.y = 10;
-  wall.scaling.z = 1;
+  wall.scaling.x = wallSizeFactor;
+  wall.scaling.y = 1.5;
+  wall.scaling.z = wallSizeFactor;
   wall.material = new BABYLON.StandardMaterial('texture1', scene);
   wall.material.emissiveTexture = new BABYLON.Texture('assets/tron1.jpg', scene);
-  wall.setEnabled(false); // this removes root mesh
+  wall.setEnabled(false); // removes root mesh
+
+  // create a roof
+  const roofSizeFactor = wallSizeFactor;
+  const roof = BABYLON.Mesh.CreateBox('plane', 1, scene); // this roof is root to make instances off of
+  roof.checkCollisions = true;
+  roof.scaling.x = roofSizeFactor;
+  roof.scaling.y = 1;
+  roof.scaling.z = roofSizeFactor;
+  roof.material = new BABYLON.StandardMaterial('texture1', scene);
+  roof.material.emissiveTexture = new BABYLON.Texture('assets/tron1.jpg', scene);
+  roof.setEnabled(false); // removes root mesh
+
   // 1 is wall
+  // 2 is player start
   const maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   ];
 
   maze.forEach((row, i) => {
     row.forEach((val, j) => {
+      const x = i * wallSizeFactor;
+      const z = j * wallSizeFactor;
       if (val === 1) {
         const wallInstance = wall.createInstance(`${i}${j}`);
-        wallInstance.position.y = 0.9;
-        wallInstance.position.x = i * wallSizeFactor;
-        wallInstance.position.z = j * wallSizeFactor;
+        wallInstance.position.y = 2.5;
+        wallInstance.position.x = x;
+        wallInstance.position.z = z;
         wallInstance.checkCollisions = true;
       }
+      if (val === 2) {
+        // Parameters : name, position, scene
+        const camera = new BABYLON.UniversalCamera('UniversalCamera', new BABYLON.Vector3(x, 2, z), scene);
+
+        // Targets the camera to a particular position. In this case the scene origin
+        camera.speed = 0.3;
+        // Attach the camera to the canvas
+        camera.attachControl(canvas, true);
+        camera.applyGravity = true;
+        camera.checkCollisions = true;
+        camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+      }
+
+      const roofInstance = roof.createInstance(`${i}${j}`);
+      roofInstance.position.y = 100;
+      roofInstance.position.x = x;
+      roofInstance.position.z = z;
+      roofInstance.checkCollisions = true;
     });
   });
 
